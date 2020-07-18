78485c5bd5e20ca5302a4ac8f5370f88aa2618a1
Brunno Costa <brunno.costa@sotreq.com.br>
2020-07-18 18:50:02 -0300

    MyGallery Screen Created

--- src/pages/homePage/homepage.js
+++ src/pages/homePage/homepage.js
@@ -2,10 +2,90 @@ import React, { Component } from 'react'
 
 import '../homePage/homepage.css'
 
-class HomePage extends Component{
+class HomePage extends Component {
 
-    render(){
-        return(<h1>HOme Page</h1>)
+    render() {
+        return (
+
+            <div>
+
+                <div className='leftMenu'>
+                    <ul>
+                        <li>Upload</li>
+                        <li>Configurações</li>
+                        <li>Sair</li>
+
+                    </ul>
+                </div>
+
+                <div className="wrapper">
+                    <h1>My Gallery</h1>
+                    <div className='Gallery'>
+                        <figure>
+                            <figcaption>
+
+                            </figcaption>
+                        </figure>
+
+                        <figure>
+                            <figcaption>
+
+                            </figcaption>
+                        </figure>
+
+                        <figure>
+                            <figcaption>
+
+                            </figcaption>
+                        </figure>
+
+                        <figure>
+                            <figcaption>
+
+                            </figcaption>
+                        </figure>
+
+                        <figure>
+                            <figcaption>
+
+                            </figcaption>
+                        </figure>
+
+                        <figure>
+                            <figcaption>
+
+                            </figcaption>
+                        </figure>
+
+                        <figure>
+                            <figcaption>
+
+                            </figcaption>
+                        </figure>
+
+
+                    </div>
+
+
+
+
+                    <div className='pagination'>
+                        <button>Anterior</button>
+                  
+                        <button>Próxima</button>
+                    </div>
+                </div>
+
+
+
+
+
+
+            </div>
+
+
+
+        )
     }
 }
 
