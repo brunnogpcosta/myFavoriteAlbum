a8642e8fd6d711064bdc5705366dfe4f90599635
Brunno Costa <brunno.costa@sotreq.com.br>
2020-08-15 13:57:59 -0300

    ajusting align content

--- src/pages/Login/login.js
+++ src/pages/Login/login.js
@@ -8,18 +8,18 @@ class Login extends Component {
 
     render() {
         return (
-            <div className='container'>
+            <div className='containerLogin'>
 
                 <form className='formLogin'>
                     <h2>Login</h2>
                     <input type="email" id="username" name="username" placeholder='E-mail' required />
                     <br />
 
-                    <input type="password" id="pwd" name="pwd" placeholder='Senha' required/>
+                    <input type="password" id="pwd" name="pwd" placeholder='Senha' required />
                     <br />
 
                     <p>Não tenho Cadastro</p>
-                    
+
                     <input type="submit" id='envi' value='Entrar' />
                 </form>
 
